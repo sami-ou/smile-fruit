5cb02196cf17649aa0875c9b9ee7f201c37d6295
sami-ou <78302181+sami-ou@users.noreply.github.com>
2021-07-09 17:21:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-ScrollReveal().reveal('.more',{delay:500});
-ScrollReveal().reveal('.post-banner',{delay:500});
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+    document.querySelector('.nav-main').classList.toggle('show')
+});
+ScrollReveal().reveal('.h1');
+ScrollReveal().reveal('.post-banner-img');
+ScrollReveal().reveal('.art1');
+ScrollReveal().reveal('.more-title');
+ScrollReveal().reveal('.card');
